cb89293074a804ff5f10d36fdf1792e5fa980873
Marlon Liemann <awesomemarlon@gmail.com>
2021-06-28 10:09:18 -0400

    added questions

--- index.js
+++ index.js
@@ -5,7 +5,56 @@ const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+      type: 'input',
+      name: 'github',
+      message: "What is your GitHub username?",
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: "What is your email address?",
+    },
+    {
+      type: 'input',
+      name: 'title',
+      message: "What is your project's name?",
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: "Write a short project descrition",
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: "What kind of license should your project have?",
+      choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: "What command should be run to install dependencies?",
+      default: 'npm i',
+    },
+    {
+      type: 'input',
+      name: 'test',
+      message: "What command should be run to run tests?",
+      default: 'npm test',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: "Does the user need to know anything regarding running the repo?",
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: "What does the user need to know about contributing to the repo?",
+    },
+  ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
